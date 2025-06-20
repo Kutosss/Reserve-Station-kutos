b2d5d9087d0eaf53bbfc2015435dcca4ca13271f
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-06-20 18:02:00 +0000

    chore: Automatically update REUSE headers

--- Content.Client/_White/Overlays/CRTVisionSystem.cs
+++ Content.Client/_White/Overlays/CRTVisionSystem.cs
@@ -1,3 +1,9 @@
+// SPDX-FileCopyrightText: 2025 Kutosss <162154227+Kutosss@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 ReserveBot <211949879+ReserveBot@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Spatison <137375981+Spatison@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared._White.Overlays;
 using Content.Shared.Damage;
 using Content.Shared.Mobs;
